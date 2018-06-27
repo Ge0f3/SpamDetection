ebb8b06b726022e57306af0a0ca04dd336d223d0
= <geoffrey.geofe@gmail.com>
2018-06-27 11:15:29 -0500

    spam detection model

---
+++ main.py
@@ -0,0 +1,38 @@
+import numpy as np 
+from flask import jsonify,abort,request,Flask
+from flask import Flask, render_template, request
+import pickle,joblib
+import json
+
+
+my_predict = pickle.load(open('model_file','rb'))
+ser_countvect =pickle.load(open('countvect','rb'))
+
+#using joblib
+# my_predict = joblib.load('model.pkl')
+# ser_countvect=joblib.load('countvect.pkl')
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/api', methods=['POST'])
+def get_delay():
+    result=request.form
+
+    productname = result['email']
+    # we create a json object that will hold data from user inputs
+    product = [productname]
+    x_count = ser_countvect.transform(product)
+    # encode the json object to one hot encoding so that it could fit our model
+    # get the price prediction
+    res = my_predict.predict(x_count)
+    #output = res[0]
+    # return a json value
+    app.logger.info("The type of the producname is {}".format(res))
+    return json.dumps({'result':res[0]});
+
+if __name__ == '__main__':
+	app.run(debug=True,host='0.0.0.0')
