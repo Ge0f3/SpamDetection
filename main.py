687bb77f3d6a1287c62c95117d62751c47fb7ef8
= <geoffrey.geofe@gmail.com>
2018-06-27 11:35:39 -0500

    spam ML model

--- main.py
+++ main.py
@@ -35,4 +35,6 @@ def get_delay():
     return json.dumps({'result':res[0]});
 
 if __name__ == '__main__':
-	app.run(debug=True,host='0.0.0.0')
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
