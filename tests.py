599c77f10695b486285de5b42ee2ea791f805066
= <geoffrey.geofe@gmail.com>
2018-06-27 21:00:44 -0500

    unit test cases

--- tests.py
+++ tests.py
@@ -1,24 +1,47 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import unittest
-import json,requests
+import json
+from main import app
+
+
 class TestMethods(unittest.TestCase):
-	"""docstring for TestMethods"""
-	def test(self):
-		url= ' http://0.0.0.0:5000/test'
-		data = json.dumps(['Congratulations ur awarded either å£500 of CD '])
-		r=requests.post(url,data)
-		result = r.json()
-		self.assertEqual(result['results'],'spam')
-	def test1(self):
-		url= ' http://0.0.0.0:5000/test'
-		data = json.dumps(['Hi how are you '])
-		r=requests.post(url,data)
-		result = r.json()
-		self.assertEqual(result['results'],'ham')
-	def test3(self):
-		url= ' http://0.0.0.0:5000/test'
-		data = json.dumps(['   '])
-		r=requests.post(url,data)
-		result = r.json()
-		self.assertEqual(result['results'],'ham')
+
+    """docstring for TestMethods"""
+
+    def setUp(self):
+        test = app.test_client(self)
+        response = test.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def post(self):
+        test = app.test_client(self)
+        response = test.post('/api', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def test1(self):
+        test = app.test_client(self)
+        data = \
+            json.dumps(['Congratulations ur awarded either \xc3\xa5\xc2\xa3500 of CD '
+                       ])
+        response = test.post('/test', data=data)
+        result = json.loads(response.data)
+        self.assertEqual(result['results'], 'spam')
+
+    def test2(self):
+        test = app.test_client(self)
+        data = json.dumps(['Hi how are you '])
+        response = test.post('/test', data=data)
+        result = json.loads(response.data)
+        self.assertEqual(result['results'], 'ham')
+
+    def test3(self):
+        test = app.test_client(self)
+        data = json.dumps(['  '])
+        response = test.post('/test', data=data)
+        result = json.loads(response.data)
+        self.assertEqual(result['results'], 'ham')
+
+
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
